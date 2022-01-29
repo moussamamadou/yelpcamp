c1f8e15d06372c733257c76d443593562baecef0
Moussa Mamadou <moussa.mamadou@outlook.com>
2022-01-29 23:50:45 +0100

    Configure _app.tsx
    - Add ChakraProvider so that can use chakra ui components
    - Add ApolloProvider so we can ask to graphql API via apolloClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const apolloClient = new ApolloClient({
+  uri: "/api/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
